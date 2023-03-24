fff471447d5be73579fa5a3fdc94894f3ab24301
Harsh Vardhan <harshv2511@gmail.com>
2023-03-24 12:14:31 +0530

    Making the lines stay

--- app/src/main/java/com/example/whiteboardapp/DrawingView.kt
+++ app/src/main/java/com/example/whiteboardapp/DrawingView.kt
@@ -16,6 +16,8 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private var color = Color.BLACK
     private var canvas: Canvas? = null
 
+    private val mPaths = ArrayList<CustomPath>()    //Stores the lines which are drawn
+
     init{
         setUpDrawing()
     }
@@ -41,6 +43,13 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!, 0f,0f, mCanvasPaint)
+
+        for(path in mPaths){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+        }
+
         if(!mDrawPath!!.isEmpty) {
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
             mDrawPaint!!.color = mDrawPath!!.color
@@ -74,6 +83,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
             }
 
             MotionEvent.ACTION_UP -> {
+                mPaths.add(mDrawPath!!)
                 mDrawPath = CustomPath(color, mBrushSize)
             }
             else -> return false
