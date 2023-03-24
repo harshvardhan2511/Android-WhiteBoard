762af2086de04d62c3c90b8e38d3a1b4738891db
Harsh Vardhan <harshv2511@gmail.com>
2023-03-25 00:32:55 +0530

    Adding Custom pallet and using linearlayout as an array of ImageButton

--- app/src/main/java/com/example/whiteboardapp/MainActivity.kt
+++ app/src/main/java/com/example/whiteboardapp/MainActivity.kt
@@ -4,16 +4,29 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
+    //I will use linear layout in main activity as an array of colors instead of their ids
+    private var mImageButtonCurrentPaint: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+
+
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+        mImageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.palette_pressedl)
+        )
+
 
         val ib_brush : ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener{
