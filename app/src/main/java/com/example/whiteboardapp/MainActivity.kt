5f5ca4d04d1c78ff839420a3d08095afd98b23ca
Harsh Vardhan <harshv2511@gmail.com>
2023-03-26 00:20:22 +0530

    Adding permission request to gallery icon/button

--- app/src/main/java/com/example/whiteboardapp/MainActivity.kt
+++ app/src/main/java/com/example/whiteboardapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.whiteboardapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +9,9 @@ import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -15,6 +20,31 @@ class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
     //I will use linear layout in main activity as an array of colors instead of their ids
     private var mImageButtonCurrentPaint: ImageButton? = null
+
+    val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+            permissions ->
+            permissions.entries.forEach{
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if(isGranted){
+                    Toast.makeText(
+                        this@MainActivity,
+                        "You can now read file storage",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }else{
+                    if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Oops you denied the permission",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +63,11 @@ class MainActivity : AppCompatActivity() {
         val ib_brush : ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener{
             showBrushSizeChooserDialog()
+        }
+
+        val ib_gallery : ImageButton = findViewById((R.id.ib_gallery))
+        ib_gallery.setOnClickListener(){
+            requestStoragePermission()
         }
     }
 
@@ -79,5 +114,44 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+    private fun requestStoragePermission(){
+        // Check if the permission was denied and show rationale
+        if (
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+        ){
+            //call the rationale dialog to tell the user why they need to allow permission request
+            showRationaleDialog("WhiteBoard","WhiteBoard " +
+                    "needs to Access Your External Storage")
+        }
+        else {
+            // You can directly ask for the permission.
+            //if it has not been denied then request for permission
+            //  The registered ActivityResultCallback gets the result of this request.
+            requestPermission.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            )
+        }
+
+    }
+
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
+
 
 }
